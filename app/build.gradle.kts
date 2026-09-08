c31ce8f5e420f182b030802c8fc15c1c72f57798
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-09-08 15:12:22 +0300

    Add androidx.core:core-ktx dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,11 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+     
+    implementation "androidx.core:core-ktx:1.12.0"
+    
+    
+
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
