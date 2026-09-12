7bc395e9323e2b9a94b50587263b0a52b3e1911b
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-09-12 22:20:50 +0300

    Change GeckoView dependency to an older version
    Updated GeckoView dependency version in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     // JSON
     implementation("com.google.code.gson:gson:2.10.1")
 // GeckoView
-implementation("org.mozilla.geckoview:geckoview:155.0.20260903215306")
-
+implementation("org.mozilla.geckoview:geckoview:130.0.20240913135723")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
 }
